de13e82bbe0b2c74b92bece6a8b600a8e8d0ab00
Jonathan Herrera <jonathan@magdalenamedio.net>
2018-02-22 14:33:02 -0500

    registrese 90%

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -2,6 +2,12 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon//favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 	<title>@yield('title', 'Raiz') | DataBlog - &copy; <?php echo date('Y'); ?></title>
 	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
@@ -11,6 +17,11 @@
 <body>
 
 	@include('template.partials.nav')
+
+
+	<section>
+		@yield('content')
+	</section>
 
 
 	@include('template.partials.footer')
--- resources/views/template/user/create.blade.php
+++ resources/views/template/user/create.blade.php
@@ -1,1 +1,39 @@
 @extends('template.main')
+@section('title', 'Crear Usuario')
+@section('content')
+<div class="bc-usuarios-registrese no-gutters ">
+   <div class="container" >
+      <div class="row justify-content-center">
+         <div class="col-sm-12 col-md-10 col-lg-8 registrese_form">
+            <h1>REGISTRARSE</h1>
+            <p>Regístrese en nuestra plataforma para
+               tener el acceso completo a los proyectos
+            </p>
+            {!! Form::open(['route' => 'user.store', 'method' => 'post', 'class' => 'myform']) !!}
+            <div class="form-group">
+               {!! Form::text('name', null, ['class' => 'form-control myinput', 'placeholder' => 'Nombre Completo' ,'required']) !!}
+            </div>
+            <div class="form-group">
+               {!! Form::email('email', null, ['class' => 'form-control myinput', 'placeholder' => 'CORREO ELECTRÓNICO' ,'required']) !!}
+            </div>
+            <div class="form-group">
+               {!! Form::password('password', ['class' => 'form-control myinput', 'placeholder' => 'CONTRASEÑA' ,'required']) !!}
+            </div>
+            <div class="form-group">
+               {!! Form::submit('REGISTRARSE', ['class' => 'form-control mysubmit']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+            <p>¿YA TIENES UNA CUENTA? - <a href="">ACCEDER</a></p>
+
+            <span></span>
+
+            <a href="" class="facebook"><i class="fab fa-facebook-f"></i> INGRESAR CON FACEBOOK</a>
+
+
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
