04a88f748ad56e3e4f7dca80f38dcb81ea8f1994
Jonathan Herrera <jonathan@magdalenamedio.net>
2018-02-22 06:59:32 -0500

    footer ok

--- resources/views/template/partials/footer.blade.php
+++ resources/views/template/partials/footer.blade.php
@@ -1,3 +1,32 @@
-<div class="card-footer text-muted">
-    2 days ago
- </div>
+<div class="container-fluid footers">
+  <div class="row footer1 container">
+
+    <div class="col-lg-3 col-md-3 col-sm-12">
+      <h2>Contacto</h2>
+      <a href="mailto:info@datablog.com">info@datablog.com</a>
+      <p>+ 57 3204280843</p>
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12">
+      <h2>MAPA DEL SITIO</h2>
+      <ul>
+      	<li><a href=""><i class="fas fa-caret-right"></i>Categorías</a></li>
+      	<li><a href=""><i class="fas fa-caret-right"></i>Proyectos</a></li>
+      	<li><a href=""><i class="fas fa-caret-right"></i>FAQ</a></li>
+      </ul>
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12">
+      
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12 redes">
+      <a href=""><img src="{{ asset('img/face.png') }}" name="Facebook"></a>
+      <a href=""><img src="{{ asset('img/gmore.png') }}" name="Google +"></a>
+      <a href=""><img src="{{ asset('img/Twotter.png') }}" name="Twitter"></a>
+    </div>
+  </div>
+  <div class="row footer2">
+  	<h6>DATABLOG | <?php echo date('Y'); ?> - <b style="color: #003152;">&copy;</b> ALL RIGHTS RESERVED</h6>   
+  </div>	
+</div>
