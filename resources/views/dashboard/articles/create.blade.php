4e2a6e2e0388e9f6c6c089f419a293126fa3abbc
Jonathan Herrera <jonathan@magdalenamedio.net>
2018-03-23 05:43:34 -0500

    requerido

--- resources/views/dashboard/articles/create.blade.php
+++ resources/views/dashboard/articles/create.blade.php
@@ -4,7 +4,7 @@
 <div class="bc-dashboard  no-gutters ">
    <div class="container" >
       <div class="row justify-content-center">
-         <div class="col-sm-12 col-md-12 col-lg-12 dashboard">
+         <div class="col-sm-12 col-md-12 col-lg-12 dashboard ">
          	<div class="row mensaje_data">
             @include('flash::message')
 			   @include('template.partials.erros')
